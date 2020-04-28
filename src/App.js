9e6ba0a0eb1b3c8b761fb0e03d154e7835e83a19
Eugene Tsvetkov <orion.octupus@gmail.com>
2020-04-28 18:59:12 +0300

    Lesson 20 NavLink, activeClassName

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
-
-import { BrowserRouter, Route } from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import s from './Nav.module.css';
+import { NavLink } from 'react-router-dom';
 
 function Nav() {
     return (
         <nav className={s.nav}>
             <ul>
-                <li><a href="/Profile" className={s.link}>Profile</a></li>
-                <li><a href="/Dialogs" className={s.link}>Messages</a></li>
-                <li><a href="/News" className={s.link}>News</a></li>
-                <li><a href="/Music" className={s.link}>Music</a></li>
-                <li><a href="/Settings" className={s.link}>Settings</a></li>
+                <li><NavLink to="/Profile" activeClassName={s.active} className={s.link}>Profile</NavLink></li>
+                <li><NavLink to="/Dialogs" activeClassName={s.active} className={s.link}>Messages</NavLink></li>
+                <li><NavLink to="/News" activeClassName={s.active} className={s.link}>News</NavLink></li>
+                <li><NavLink to="/Music" activeClassName={s.active} className={s.link}>Music</NavLink></li>
+                <li><NavLink to="/Settings" activeClassName={s.active} className={s.link}>Settings</NavLink></li>
             </ul>
         </nav>
     );
