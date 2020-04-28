0487db01bb63cf1eb951cbc247a465c3eccf1e52
Eugene Tsvetkov <orion.octupus@gmail.com>
2020-04-28 21:10:02 +0300

    Lesson 22 Dialogs NavLink, Route

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
 
 function Dialogs() {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={`${s.dialog} ${s.active}`}>
-                    Andrew
+                <div className={`${s.dialog}`}>
+                    <NavLink to="/Dialogs/1" activeClassName={s.active} className={s.link}>Andrew</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Anatoly
+                    <NavLink to="/Dialogs/2" activeClassName={s.active} className={s.link}>Anatoly</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Ivan
+                    <NavLink to="/Dialogs/3" activeClassName={s.active} className={s.link}>Ivan</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Marina
+                    <NavLink to="/Dialogs/4" activeClassName={s.active} className={s.link}>Marina</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Pavel
+                    <NavLink to="/Dialogs/5" activeClassName={s.active} className={s.link}>Pavel</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Lena
+                    <NavLink to="/Dialogs/6" activeClassName={s.active} className={s.link}>Lena</NavLink>
                 </div>
             </div>
             <div className={s.messages}>
