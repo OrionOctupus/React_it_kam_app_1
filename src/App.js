c4510016cdbd65cea20ba8233ca189b84a95dac0
Eugene Tsvetkov <orion.octupus@gmail.com>
2020-04-28 14:40:24 +0300

    Lesson 18  component Dialogs

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
 
 function App() {
   return (
     <div className="app-wrapper">
       <Header />
       <Nav />
-      <Profile />
+      <div className="app-wrapper__content">
+        {/* <Profile /> */}
+        <Dialogs />
+      </div>
     </div>
   );
 }
---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Dialogs.module.css';
+
+function Dialogs() {
+    return (
+        <div className={s.content}>
+            <p>Dialogs</p>
+        </div>
+    );
+}
+
+export default Dialogs;
