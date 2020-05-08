bd38d674d13d2cae04e254b5fcff0a46e8ff2e64
Eugene Tsvetkov <orion.octupus@gmail.com>
2020-05-08 23:11:45 +0300

    Lesson 39. action creator, action type

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
+import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/state'
+
 
 function MyPosts(props) {
     let newPostElement = React.createRef();
 
     let addPostMessage = () => {
-        props.dispatch({ type: 'ADD-POST' });
+        props.dispatch(addPostActionCreator());
         // props.updateNewPostText('');
         // newPostElement.current.value = "";
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        props.dispatch({ type: 'UPDATE-NEW-POST-TEXT', newText: text });
+        props.dispatch(updateNewPostTextActionCreator(text));
     }
     let message = props.state.profilePage.messagesPost
         .map(m => <Post message={m.message} like={m.like} />)
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,7 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
+
 let store = {
     _state: {
         dialogsPage: {
@@ -67,7 +71,7 @@ let store = {
     // -->  action{type:'text'} - объект с описанием действия;
 
     dispatch(action) {
-        if (action.type === 'ADD-POST') {
+        if (action.type === ADD_POST) {
             let newPost = {
                 id: 5,
                 message: this._state.profilePage.newPostText,
@@ -76,12 +80,21 @@ let store = {
             this._state.profilePage.messagesPost.push(newPost);
             this._state.profilePage.newPostText = '';
             this._callSubscriber(this._state);
-        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+        } else if (action.type === UPDATE_NEW_POST_TEXT) {
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
         }
     }
 }
+
+export const addPostActionCreator = () => ({ type: 'ADD-POST' });
+
+export const updateNewPostTextActionCreator = (text) => {
+    return {
+        type: 'UPDATE-NEW-POST-TEXT',
+        newText: text
+    }
+};
 
 export default store;
 window.store = store;
