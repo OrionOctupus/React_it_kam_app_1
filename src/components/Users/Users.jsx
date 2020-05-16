3f96c8c83b1b2cce535522c1c9b8950f80eafaad
Eugene Tsvetkov <orion.octupus@gmail.com>
2020-05-16 23:08:44 +0300

    Lesson 51. lib axios. GET-request

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import s from './Users.module.css';
+import * as axios from 'axios';
+import userAvatarDefault from '../../../src/assets/images/userAvatarDefault.png';
 
 
 function Users(props) {
-
     if (props.users.length === 0) {
-        props.setUsers([
-            { id: 1, followed: false, fullName: "Eugene", status: 'I am learn React&Redux', location: { city: 'Moscow', country: 'Russia' }, photoUrl: 'https://assets.vg247.com/current//2014/11/human_element_nov_14.jpg' },
-            { id: 2, followed: true, fullName: "Elena", status: 'I am learn React&Redux', location: { city: 'Kiev', country: 'Ukrane' }, photoUrl: 'https://assets.vg247.com/current//2014/11/human_element_nov_14.jpg' },
-            { id: 3, followed: false, fullName: "Pavel", status: 'I am learn React&Redux', location: { city: 'Minsk', country: 'Belarus' }, photoUrl: 'https://assets.vg247.com/current//2014/11/human_element_nov_14.jpg' },
-            { id: 4, followed: true, fullName: "Victor", status: 'I am learn React&Redux', location: { city: 'Omsk', country: 'Russia' }, photoUrl: 'https://assets.vg247.com/current//2014/11/human_element_nov_14.jpg' },
-        ]);
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                props.setUsers(response.data.items);
+            })
     }
+    // if (props.users.length === 0) {
+    //     props.setUsers([
+    //         { id: 1, followed: false, fullName: "Eugene", status: 'I am learn React&Redux', location: { city: 'Moscow', country: 'Russia' }, photoUrl: 'https://assets.vg247.com/current//2014/11/human_element_nov_14.jpg' },
+    //         { id: 2, followed: true, fullName: "Elena", status: 'I am learn React&Redux', location: { city: 'Kiev', country: 'Ukrane' }, photoUrl: 'https://assets.vg247.com/current//2014/11/human_element_nov_14.jpg' },
+    //         { id: 3, followed: false, fullName: "Pavel", status: 'I am learn React&Redux', location: { city: 'Minsk', country: 'Belarus' }, photoUrl: 'https://assets.vg247.com/current//2014/11/human_element_nov_14.jpg' },
+    //         { id: 4, followed: true, fullName: "Victor", status: 'I am learn React&Redux', location: { city: 'Omsk', country: 'Russia' }, photoUrl: 'https://assets.vg247.com/current//2014/11/human_element_nov_14.jpg' },
+    //     ]);
+    // }
 
     return (
         <div>
@@ -21,7 +28,7 @@ function Users(props) {
                         <div key={u.id}>
                             <span>
                                 <div>
-                                    <img className={s.usersPhoto} src={u.photoUrl} />
+                                    <img className={s.usersPhoto} src={u.photos.small != null ? u.photos.small : userAvatarDefault} />
                                 </div>
                                 <div>
                                     {u.followed
@@ -31,11 +38,11 @@ function Users(props) {
                             </span>
                             <span>
                                 <span>
-                                    <div>{u.fullName}</div><div>{u.status}</div>
+                                    <div>{u.name}</div><div>{u.status}</div>
                                 </span>
                                 <span>
-                                    <div>{u.location.country}</div>
-                                    <div>{u.location.city}</div>
+                                    <div>{"u.location.country"}</div>
+                                    <div>{"u.location.city"}</div>
                                 </span>
                             </span>
                         </div>
