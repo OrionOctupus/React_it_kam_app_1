68773960dfc59d8b1e2285ab4b5cb16100732cc8
Eugene Tsvetkov <orion.octupus@gmail.com>
2020-05-22 22:30:32 +0300

    Lesson 61. authReducer

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,14 @@ import profilePageReducer from "./profilePageReducer";
 import dialogsPageReducer from "./dialogsPageReducer";
 import navPageReducer from "./navPageReducer";
 import usersPageReducer from "./usersPageReducer";
+import authReducer from "./auth-reducer";
 
 let reducers = combineReducers({
     profilePage: profilePageReducer,
     dialogsPage: dialogsPageReducer,
     navPage: navPageReducer,
     usersPage: usersPageReducer,
+    auth: authReducer,
 });
 
 let store = createStore(reducers);
